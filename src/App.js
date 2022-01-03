6bb32803d8f82ff6a5d8fc0d0f42092ec8effbdc
tejassohoni <tjs1597@gmail.com>
2022-01-03 21:55:33 +0530

    Added header to the page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Header from "./Header";
 function App() {
   return (
     <div className="app">
-      {/* Header */}
       <Header />
       {/* App Body */}
       {/* Sidebar */}
--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,40 @@
 import React from "react";
+
 import "./Header.css";
+
+import HeaderOption from "./HeaderOption";
+
+import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import SupervisorAccountSharpIcon from "@material-ui/icons/SupervisorAccountSharp";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 
 function Header() {
   return (
     <div className="header">
-      <h1>This is a header</h1>
-      <div className="header_left">
-        <img src="" />
-        <div className="header_search">
-          {/* Search Icon */}
+      <div className="header__left">
+        <img
+          alt=""
+          src="https://upload.wikimedia.org/wikipedia/commons/8/81/LinkedIn_icon.svg"
+        />
+        <div className="header__search">
+          <SearchIcon />
           <input type="text" />
         </div>
       </div>
-      <div className="header_right"></div>
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountSharpIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption
+          avatar="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/PjxzdmcgaGVpZ2h0PSI0OCIgdmlld0JveD0iMCAwIDQ4IDQ4IiB3aWR0aD0iNDgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTI0IDRjLTExLjA1IDAtMjAgOC45NS0yMCAyMHM4Ljk1IDIwIDIwIDIwIDIwLTguOTUgMjAtMjAtOC45NS0yMC0yMC0yMHptMCA2YzMuMzEgMCA2IDIuNjkgNiA2IDAgMy4zMi0yLjY5IDYtNiA2cy02LTIuNjgtNi02YzAtMy4zMSAyLjY5LTYgNi02em0wIDI4LjRjLTUuMDEgMC05LjQxLTIuNTYtMTItNi40NC4wNS0zLjk3IDguMDEtNi4xNiAxMi02LjE2czExLjk0IDIuMTkgMTIgNi4xNmMtMi41OSAzLjg4LTYuOTkgNi40NC0xMiA2LjQ0eiIvPjxwYXRoIGQ9Ik0wIDBoNDh2NDhoLTQ4eiIgZmlsbD0ibm9uZSIvPjwvc3ZnPg=="
+          title="Me"
+        />
+      </div>
     </div>
   );
 }
